bcdd50d8a9f5d31cfdca7c11071499a20d5dcbd2
xutongle <xutongle@gmail.com>
2018-02-26 18:39:19 +0800

    tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+if (PHP_MAJOR_VERSION>=7 && PHP_MINOR_VERSION>=1) {
+    // skip deprecation errors in PHP 7.1 and above
+    error_reporting(E_ALL & ~E_DEPRECATED);
+}
+defined('YII_ENABLE_EXCEPTION_HANDLER') or define('YII_ENABLE_EXCEPTION_HANDLER',false);
+defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER',false);
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+$_SERVER['SCRIPT_NAME']='/'.basename(__FILE__);
+$_SERVER['SCRIPT_FILENAME']=__FILE__;
+
+
+
+$_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
+$_SERVER['SCRIPT_FILENAME'] = __FILE__;
+
+require __DIR__.'/../vendor/autoload.php';
