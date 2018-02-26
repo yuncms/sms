437c2ae3a4778bffe5968ef1eb18d2bbb0e44344
xutongle <xutongle@gmail.com>
2018-02-26 18:13:44 +0800

    资源

---
+++ resources/lib/jquery-treegrid/js/jquery.treegrid.bootstrap2.js
@@ -0,0 +1,2 @@
+$.extend($.fn.treegrid.defaults,{expanderTemplate:'<i class="treegrid-expander"></i>',expanderExpandedClass:"icon-chevron-down",expanderCollapsedClass:"icon-chevron-right"});
+//# sourceMappingURL=jquery.treegrid.bootstrap2.js.map
---
+++ resources/lib/jquery-treegrid/js/jquery.treegrid.bootstrap3.js
@@ -0,0 +1,2 @@
+$.extend($.fn.treegrid.defaults,{expanderExpandedClass:"glyphicon glyphicon-chevron-down",expanderCollapsedClass:"glyphicon glyphicon-chevron-right"});
+//# sourceMappingURL=jquery.treegrid.bootstrap3.js.map
