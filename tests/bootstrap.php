a42ebfe0f36d568ce9aaa606e45f557d3b2d4d5b
xutongle <xutongle@gmail.com>
2018-02-27 10:32:04 +0800

    tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,12 +12,9 @@ if (PHP_MAJOR_VERSION>=7 && PHP_MINOR_VERSION>=1) {
 defined('YII_ENABLE_EXCEPTION_HANDLER') or define('YII_ENABLE_EXCEPTION_HANDLER',false);
 defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER',false);
 defined('YII_DEBUG') or define('YII_DEBUG',true);
-$_SERVER['SCRIPT_NAME']='/'.basename(__FILE__);
-$_SERVER['SCRIPT_FILENAME']=__FILE__;
-
-
 
 $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
 require __DIR__.'/../vendor/autoload.php';
+require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
