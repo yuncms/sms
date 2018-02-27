628675f03f94099bb4d46662173f6822f092c493
xutongle <xutongle@gmail.com>
2018-02-27 16:14:22 +0800

    update

--- src/helpers/StringHelper.php
+++ src/helpers/StringHelper.php
@@ -7,6 +7,9 @@
 
 namespace yuncms\helpers;
 
+use Yii;
+use Stringy\Stringy as BaseStringy;
+
 /**
  * Class StringHelper
  *
@@ -15,6 +18,29 @@ namespace yuncms\helpers;
  */
 class StringHelper extends \yii\helpers\StringHelper
 {
+    /**
+     * @var
+     */
+    private static $_asciiCharMap;
+
+    /**
+     * Returns ASCII character mappings, merging in any custom defined mappings from the 'customAsciiCharMappings'
+     * config setting.
+     *
+     * @return array The fully merged ASCII character mappings.
+     */
+    public static function asciiCharMap(): array
+    {
+        if (self::$_asciiCharMap !== null) {
+            return self::$_asciiCharMap;
+        }
+
+        // Get the map from Stringy.
+        self::$_asciiCharMap = (new Stringy(''))->getAsciiCharMap();
+
+        return self::$_asciiCharMap;
+    }
+
     /**
      * 提取两个字符串之间的值，不包括分隔符
      *
@@ -75,5 +101,43 @@ class StringHelper extends \yii\helpers\StringHelper
     public static function isUUID(string $uuid): bool
     {
         return !empty($uuid) && preg_match('/[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}/ui', $uuid);
+    }
+
+    /**
+     * Converts all characters in the string to lowercase. An alias for PHP's mb_strtolower().
+     *
+     * @param string $str The string to convert to lowercase.
+     *
+     * @return string The lowercase string.
+     */
+    public static function toLowerCase(string $str): string
+    {
+        return (string)BaseStringy::create($str)->toLowerCase();
+    }
+
+    /**
+     * Converts an object to its string representation. If the object is an array, will glue the array elements togeter
+     * with the $glue param. Otherwise will cast the object to a string.
+     *
+     * @param mixed  $object The object to convert to a string.
+     * @param string $glue   The glue to use if the object is an array.
+     *
+     * @return string The string representation of the object.
+     */
+    public static function toString($object, string $glue = ','): string
+    {
+        if (is_scalar($object) || (is_object($object) && method_exists($object, '__toString'))) {
+            return (string)$object;
+        }
+        if (is_array($object) || $object instanceof \IteratorAggregate) {
+            $stringValues = [];
+            foreach ($object as $value) {
+                if (($value = static::toString($value, $glue)) !== '') {
+                    $stringValues[] = $value;
+                }
+            }
+            return implode($glue, $stringValues);
+        }
+        return '';
     }
 }
