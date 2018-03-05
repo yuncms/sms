1f972f99a4b635477c15e2cbddeaffe6e46774d0
xutongle <xutongle@gmail.com>
2018-03-05 09:52:33 +0800

    队列

--- migrations/m180305_014442_create_queue_table.php
+++ migrations/m180305_014442_create_queue_table.php
@@ -7,14 +7,35 @@ use yii\db\Migration;
  */
 class m180305_014442_create_queue_table extends Migration
 {
+    public $tableName = '{{%queue}}';
+
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
-        $this->createTable('queue', [
-            'id' => $this->primaryKey(),
-        ]);
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable($this->tableName, [
+            'id' => $this->bigPrimaryKey(),
+            'channel' => $this->string()->notNull(),
+            'job' => $this->binary()->notNull(),
+            'pushed_at' => $this->integer()->unsigned()->notNull(),
+            'ttr'=>$this->integer()->notNull()->unsigned(),
+            'delay' => $this->integer()->unsigned()->defaultValue(0)->notNull(),
+            'priority'=>$this->integer()->unsigned()->notNull()->defaultValue(1024),
+            'reserved_at' => $this->integer()->unsigned(),
+            'attempt'=>$this->integer()->unsigned(),
+            'done_at' => $this->integer()->unsigned(),
+        ], $tableOptions);
+
+        $this->createIndex('channel', $this->tableName, 'channel');
+        $this->createIndex('reserved_at', $this->tableName, 'reserved_at');
+        $this->createIndex('priority', $this->tableName, 'priority');
     }
 
     /**
@@ -22,6 +43,6 @@ class m180305_014442_create_queue_table extends Migration
      */
     public function safeDown()
     {
-        $this->dropTable('queue');
+        $this->dropTable($this->tableName);
     }
 }
