5845ad2bd8b40f03e54fd960222f26f39f85673c
xutongle <xutongle@gmail.com>
2018-03-06 14:18:22 +0800

    init Payment

---
+++ src/payment/Trade.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\payment;
+
+
+/**
+ * Class Trade
+ * @package yuncms\payment
+ * @author Tongle Xu <xutongle@gmail.com>
+ */
+class Trade
+{
+
+}
