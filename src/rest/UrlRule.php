5f5d31e862b634ca0a1ca8945c04a855ed131d39
xutongle <xutongle@gmail.com>
2018-03-07 17:09:45 +0800

    扩展 UrlRule

---
+++ src/rest/UrlRule.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\rest;
+
+
+/**
+ * Class UrlRule
+ * @package yuncms\rest
+ * @author Tongle Xu <xutongle@gmail.com>
+ */
+class UrlRule extends \yii\rest\UrlRule
+{
+    /**
+     * @var array tokens for supporting extra actions in addition to those listed in [[tokens]].
+     * The keys are the tokens and the values are the corresponding action IDs.
+     * These extra patterns will take precedence over [[tokens]].
+     */
+    public $extraTokens = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+        $this->tokens = $this->extraTokens + $this->tokens;
+    }
+}
