b6322f8409797a6c9b156b39e20378339e7eb8c9
xutongle <xutongle@gmail.com>
2018-03-08 19:34:22 +0800

    update

--- src/payment/gateways/Gateway.php
+++ src/payment/gateways/Gateway.php
@@ -20,5 +20,33 @@ class Gateway implements GatewayInterface
 {
     use GatewayTrait;
 
+    const DEFAULT_TIMEOUT = 5.0;
 
+    /**
+     * @var float
+     */
+    protected $timeout;
+
+    /**
+     * Return timeout.
+     *
+     * @return int|mixed
+     */
+    public function getTimeout()
+    {
+        return $this->timeout ?: self::DEFAULT_TIMEOUT;
+    }
+
+    /**
+     * Set timeout.
+     *
+     * @param int $timeout
+     *
+     * @return $this
+     */
+    public function setTimeout($timeout)
+    {
+        $this->timeout = floatval($timeout);
+        return $this;
+    }
 }
