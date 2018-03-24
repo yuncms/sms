853b2e48051984831e21aa65e0dc0cf7ccc1b8e2
xutongle <xutongle@gmail.com>
2018-03-24 15:38:12 +0800

    update

--- src/payment/gateways/Gateway.php
+++ src/payment/gateways/Gateway.php
@@ -22,19 +22,37 @@ class Gateway extends Component implements GatewayInterface
 {
     use GatewayTrait;
 
+    //连接超时
     const DEFAULT_TIMEOUT = 5.0;
 
+    //交易类型
+    const TYPE_QR_CODE = 'qr-code';//扫码支付
+    const TYPE_JS_API = 'js-api';//应用内JS API,如微信
+    const TYPE_APP = 'app';//app支付
+    const TYPE_WAP = 'wap';//H5支付
+    const TYPE_WEB = 'web';//PC支付
+    const TYPE_POST = 'pos';//刷卡支付
+
+    //交易状态
+    const STATE_NOT_PAY = 0b0;//未支付
+    const STATE_SUCCESS = 0b1;//支付成功
+    const STATE_FAILED = 0b10;//支付失败
+    const STATE_CLOSED = 0b100;//已关闭
+    const STATE_REVOKED = 0b101;//已撤销
+    const STATE_ERROR = 0b110;//错误
+    const STATE_REFUND = 0b111;//转入退款
+    const STATE_REFUND_SUCCESS = 0b11;//转入退款
+    const STATE_REFUND_FAILED = 0b11;//转入退款
+
     /**
-     * @var float
+     * @var float 连接超时时间
      */
     protected $timeout;
 
     /**
      * @var array 交易类型和Trade映射
      */
-    public $tradeTypeMap = [
-
-    ];
+    public $tradeTypeMap = [];
 
     /**
      * 获取交易类型
---
+++ src/payment/models/Charge.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace vendor\yuncms\framework\src\payment\models;
+
+
+use yuncms\db\ActiveRecord;
+
+/**
+ * 收款
+ *
+ * @author Tongle Xu <xutongle@gmail.com>
+ * @since 3.0
+ */
+class Charge extends ActiveRecord
+{
+
+}
