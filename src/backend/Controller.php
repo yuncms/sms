dbeedfe87bcc2fee0dbf8fa2cfd252497bce5adb
xutongle <xutongle@gmail.com>
2018-03-24 17:18:10 +0800

    update

---
+++ src/backend/Controller.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\backend;
+
+/**
+ * Class Controller
+ *
+ * @author Tongle Xu <xutongle@gmail.com>
+ * @since 3.0
+ */
+class Controller extends \yuncms\web\Controller
+{
+
+}
