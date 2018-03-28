f8ef6d0f1ec225b735bc5c01b1911b441e0db5ed
xutongle <xutongle@gmail.com>
2018-03-28 19:11:28 +0800

    上传处理

--- src/models/Attachment.php
+++ src/models/Attachment.php
@@ -102,7 +102,7 @@ class Attachment extends ActiveRecord
      */
     public function getUser()
     {
-        return $this->hasOne(User::className(), ['id' => 'user_id']);
+        return $this->hasOne(User::class, ['id' => 'user_id']);
     }
 
     /**
