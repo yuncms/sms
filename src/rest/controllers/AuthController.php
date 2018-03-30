595e5bb483808ea7bd8f1aaf9035bf34448a757e
xutongle <xutongle@gmail.com>
2018-03-30 11:10:06 +0800

    add oauth2 auth

---
+++ src/rest/controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\rest\controllers;
+
+use Yii;
+use yuncms\web\Controller;
+use yuncms\oauth2\frontend\models\LoginForm;
+
+/**
+ * OAuth2 认证
+ * @property bool $isOauthRequest 是否是OAuth2请求
+ * @method finishAuthorization
+ */
+class AuthController extends Controller
+{
+    public $enableCsrfValidation = false;
+
+    public function behaviors()
+    {
+        return [
+            'oauth2Auth' => [
+                'class' => 'yuncms\oauth2\filters\Authorize',
+                'only' => ['authorize'],
+            ],
+        ];
+    }
+
+    public function actions()
+    {
+        return [
+            /**
+             * Returns an access token.
+             */
+            'token' => [
+                'class' => 'yuncms\oauth2\actions\Token',
+            ],
+            /**
+             * Returns an access token.
+             */
+            'qrcode' => [
+                'class' => 'yuncms\oauth2\actions\QRCode',
+            ],
+        ];
+    }
+
+    /**
+     * Display login form, signup or something else.
+     * AuthClients such as Google also may be used
+     */
+    public function actionAuthorize()
+    {
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            if ($this->isOauthRequest) {
+                $this->finishAuthorization();
+            } else {
+                return $this->goBack();
+            }
+        } else {
+            $this->layout = false;
+            return $this->render('authorize', [
+                'model' => $model,
+            ]);
+        }
+    }
+}
