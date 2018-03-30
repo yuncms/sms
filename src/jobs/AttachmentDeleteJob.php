ed50218dc04f80dbb1bca74031fa5e82ab69c03a
xutongle <xutongle@gmail.com>
2018-03-30 16:03:04 +0800

    异步删除附件

---
+++ src/jobs/AttachmentDeleteJob.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\attachment\jobs;
+
+use Yii;
+use yii\base\BaseObject;
+use yii\queue\RetryableJobInterface;
+use yii\base\Exception;
+use League\Flysystem\Filesystem;
+use yuncms\filesystem\Adapter;
+
+/**
+ * Class AttachmentDeleteJob.
+ */
+class AttachmentDeleteJob extends BaseObject implements RetryableJobInterface
+{
+    /**
+     * @var string 文件路径
+     */
+    public $path;
+
+    /**
+     * @param $queue
+     * @throws \League\Flysystem\FileNotFoundException
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function execute($queue)
+    {
+        if(self::getVolume()->has($this->path)){
+            self::getVolume()->delete($this->path);
+        }
+    }
+
+    /**
+     * 获取头像存储卷
+     * @return Adapter|Filesystem
+     * @throws \yii\base\InvalidConfigException
+     */
+    public static function getVolume()
+    {
+        return Yii::$app->getFilesystem()->get(Yii::$app->settings->get('volume', 'attachment', 'attachment'));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getTtr()
+    {
+        return 60;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function canRetry($attempt, $error)
+    {
+        return $attempt < 3;
+    }
+}
