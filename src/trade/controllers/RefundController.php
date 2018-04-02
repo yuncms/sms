04cb81236898d057680426f641c6799be30f4b56
xutongle <xutongle@gmail.com>
2018-04-02 16:01:29 +0800

    update

--- src/trade/controllers/RefundController.php
+++ src/trade/controllers/RefundController.php
@@ -20,6 +20,30 @@ use yuncms\web\Controller;
  */
 class RefundController extends Controller
 {
+    /**
+     * 创建退款单
+     * @return string|\yii\web\Response
+     */
+    public function actionCreate()
+    {
+        $model = new TradeRefunds();
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['/trade/refund/view', 'id' => $model->id]);
+        }
+        return $this->render('create', ['model' => $model]);
+    }
+
+    /**
+     * 查看退款详情
+     * @param int $id
+     * @return string
+     * @throws NotFoundHttpException
+     */
+    public function actionView($id)
+    {
+        $model = $this->findModel($id);
+        return $this->render('view', ['model' => $model]);
+    }
 
     /**
      * 获取退款单号
