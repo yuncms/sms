9ac4e09fc36a921f031d41efda88adf9dff3892d
xutongle <xutongle@gmail.com>
2018-04-26 15:01:27 +0800

    update

--- src/notifications/migrations/m180410_092555_create_notification_table.php
+++ src/notifications/migrations/m180410_092555_create_notification_table.php
@@ -25,17 +25,12 @@ class m180410_092555_create_notification_table extends Migration
         // https://segmentfault.com/q/1010000000672529/a-1020000000679702
         $this->createTable($this->tableName, [
             'id' => $this->bigPrimaryKey()->unsigned()->comment('Id'),//通知ID
+            'user_id' => $this->unsignedInteger()->comment('User Id'),//接收器
             'verb' => $this->string(32)->comment('Verb'),//活动图片
             'template' => $this->string()->comment('Template'),//通知类型
-            'channel' => $this->string(64)->comment('Template'),//通知类型
             'is_read' => $this->boolean()->defaultValue(false)->comment('Read'),//是否已读
             'is_pending' => $this->boolean()->defaultValue(false)->comment('Pending'),//是否已经推送
-            'sender_id' => $this->integer()->unsigned()->comment('Sender Id'),//发送者ID
-            'sender_class' => $this->string()->comment('Sender Class'),//发送者模型
-            'receiver_id' => $this->unsignedInteger()->comment('Receiver Id'),//接收器
-            'entity_id' => $this->unsignedInteger()->comment('Entity'),//任务对象
-            'source_id' => $this->unsignedInteger()->comment('Source'),//原有任务对象
-            'target_id' => $this->unsignedInteger()->comment('Target'),//目标对象
+            'entity' => $this->unsignedInteger()->comment('Entity'),//任务对象
             'publish_at' => $this->integer()->unsigned()->notNull()->comment('Publish At'),//发送时间
         ], $tableOptions);
 
