09aa54f37a1950d356d7b00d88aee10f089c387b
XuTongle <xutongle@gmail.com>
2018-05-11 16:16:04 +0800

    update

--- src/filesystem/FilesystemAdapter.php
+++ src/filesystem/FilesystemAdapter.php
@@ -7,13 +7,20 @@
 
 namespace yuncms\filesystem;
 
-use Yii;
+use RuntimeException;
+use yii\base\InvalidArgumentException;
+use yii\di\Instance;
 use yii\base\Component;
 use yii\base\InvalidConfigException;
 use yii\caching\Cache as YiiCache;
-use yii\di\Instance;
 use League\Flysystem\AdapterInterface;
 use League\Flysystem\Cached\CachedAdapter;
+use League\Flysystem\FileNotFoundException;
+use League\Flysystem\Filesystem as Flysystem;
+use yuncms\filesystem\contracts\CloudFilesystemInterface;
+use yuncms\filesystem\contracts\FilesystemInterface;
+use yuncms\filesystem\exceptions\FileNotFoundException as ContractFileNotFoundException;
+use yuncms\web\UploadedFile;
 
 /**
  * @mixin \League\Flysystem\FilesystemInterface
@@ -21,36 +28,32 @@ use League\Flysystem\Cached\CachedAdapter;
  * @author Tongle Xu <xutongle@gmail.com>
  * @since 3.0
  *
+ * @property string $visibility
  * @property string|false $rootUrl
  */
-abstract class FilesystemAdapter extends Component
+abstract class FilesystemAdapter extends Component implements FilesystemInterface, CloudFilesystemInterface
 {
     /**
      * The Flysystem filesystem implementation.
      *
-     * @var \League\Flysystem\FilesystemInterface
+     * @var \League\Flysystem\Filesystem
      */
-    protected $adapter;
+    protected $driver;
 
     /**
-     * @var string|Cache
+     * @var string|YiiCache
      */
-    public $cache;
+    public $cache = 'cache';
 
     /**
      * @var string
      */
-    public $cacheKey = 'flysystem';
+    public $cacheKey = 'filesystem';
 
     /**
      * @var integer
      */
-    public $cacheDuration = 3600;
-
-    /**
-     * @var string|null The adapter URL
-     */
-    public $url;
+    public $cacheDuration = 0;
 
     /**
      * 初始化适配器
@@ -59,66 +62,456 @@ abstract class FilesystemAdapter extends Component
     public function init()
     {
         parent::init();
-        $adapter = $this->prepareAdapter();
+        $driver = $this->createDriver();
         if ($this->cache !== null) {
-            /* @var Cache $cache */
-            $cache = Instance::ensure($this->cache, Cache::class);
-            if (!$cache instanceof Cache) {
+            $this->cache = Instance::ensure($this->cache, YiiCache::class);
+            if (!$this->cache instanceof YiiCache) {
                 throw new InvalidConfigException('The "cache" property must be an instance of \yii\caching\Cache subclasses.');
             }
-            $adapter = new CachedAdapter($adapter, new Cache($cache, $this->cacheKey, $this->cacheDuration));
+            $driver = new CachedAdapter($driver, new Cache($this->cache, $this->cacheKey, $this->cacheDuration));
         }
         // And use that to create the file system
-        $this->adapter = new \League\Flysystem\Filesystem($adapter);
+        $this->driver = new Flysystem($driver);
     }
-
-    /**
-     * @inheritdoc
-     */
-    abstract public static function displayName();
 
     /**
      * 准备适配器
      * @return AdapterInterface
      */
-    abstract protected function prepareAdapter();
+    abstract protected function createDriver();
 
     /**
-     * 获取Web访问Url
-     * @param string $file
-     * @return string
-     * @throws InvalidConfigException
-     */
-    public function getUrl($file)
-    {
-        if ($this->url !== null) {
-            return $this->getRootUrl() . $file;
-        } else {
-            throw new InvalidConfigException('The "url" property must be set.');
-        }
-    }
-
-    /**
-     * Returns the URL to the source, if it’s accessible via HTTP traffic.
+     * 判断文件是否存在
      *
-     * @return string|false The root URL, or `false` if there isn’t one
+     * @param  string $path
+     * @return bool
      */
-    public function getRootUrl()
+    public function exists($path)
     {
-        if ($this->url !== null) {
-            return rtrim(Yii::getAlias($this->url), '/') . '/';
-        }
-        return false;
+        return $this->driver->has($path);
     }
 
     /**
-     * 魔术方法，执行适配器方法
-     * @param string $method
-     * @param array $parameters
-     * @return mixed
+     * 获取文件的完整路径
+     *
+     * @param  string $path
+     * @return string
      */
-    public function __call($method, $parameters)
+    public function path($path)
     {
-        return call_user_func_array([$this->adapter, $method], $parameters);
+        return $this->driver->getAdapter()->getPathPrefix() . $path;
+    }
+
+    /**
+     * 获取文件的内容。
+     *
+     * @param  string $path
+     * @return string
+     *
+     * @throws ContractFileNotFoundException
+     */
+    public function get($path)
+    {
+        try {
+            return $this->driver->read($path);
+        } catch (FileNotFoundException $e) {
+            throw new ContractFileNotFoundException($path, $e->getCode(), $e);
+        }
+    }
+
+    /**
+     * Write the contents of a file.
+     *
+     * @param  string $path
+     * @param  string|resource $contents
+     * @param  mixed $options
+     * @return bool
+     */
+    public function put($path, $contents, $options = [])
+    {
+        $options = is_string($options) ? ['visibility' => $options] : (array)$options;
+
+        if ($contents instanceof UploadedFile) {
+            return $this->putFile($path, $contents, $options);
+        }
+
+        return is_resource($contents) ? $this->driver->putStream($path, $contents, $options) : $this->driver->put($path, $contents, $options);
+    }
+
+    /**
+     * Store the uploaded file on the disk.
+     *
+     * @param  string $path
+     * @param  \yuncms\web\UploadedFile $file
+     * @param  array $options
+     * @return string|false
+     */
+    public function putFile($path, $file, $options = [])
+    {
+        return $this->putFileAs($path, $file, $file->getRename(), $options);
+    }
+
+    /**
+     * Store the uploaded file on the disk with a given name.
+     *
+     * @param  string $path
+     * @param  \yuncms\web\UploadedFile $file
+     * @param  string $name
+     * @param  array $options
+     * @return string|false
+     */
+    public function putFileAs($path, $file, $name, $options = [])
+    {
+        $stream = fopen($file->tempName, 'r+');
+
+        // Next, we will format the path of the file and store the file using a stream since
+        // they provide better performance than alternatives. Once we write the file this
+        // stream will get closed automatically by us so the developer doesn't have to.
+        $result = $this->put(
+            $path = trim($path . '/' . $name, '/'), $stream, $options
+        );
+
+        if (is_resource($stream)) {
+            fclose($stream);
+        }
+
+        return $result ? $path : false;
+    }
+
+    /**
+     * 获取文件的可见性属性
+     *
+     * @param  string $path
+     * @return string
+     * @throws FileNotFoundException
+     */
+    public function getVisibility($path)
+    {
+        if ($this->driver->getVisibility($path) == AdapterInterface::VISIBILITY_PUBLIC) {
+            return FilesystemInterface::VISIBILITY_PUBLIC;
+        }
+        return FilesystemInterface::VISIBILITY_PRIVATE;
+    }
+
+    /**
+     * 设置文件可见性
+     *
+     * @param  string $path
+     * @param  string $visibility
+     * @return bool
+     * @throws FileNotFoundException
+     */
+    public function setVisibility($path, $visibility)
+    {
+        return $this->driver->setVisibility($path, $this->parseVisibility($visibility));
+    }
+
+    /**
+     * Prepend to a file.
+     *
+     * @param  string $path
+     * @param  string $data
+     * @param  string $separator
+     * @return int
+     * @throws ContractFileNotFoundException
+     */
+    public function prepend($path, $data, $separator = PHP_EOL)
+    {
+        if ($this->exists($path)) {
+            return $this->put($path, $data . $separator . $this->get($path));
+        }
+        return $this->put($path, $data);
+    }
+
+    /**
+     * Append to a file.
+     *
+     * @param  string $path
+     * @param  string $data
+     * @param  string $separator
+     * @return int
+     * @throws ContractFileNotFoundException
+     */
+    public function append($path, $data, $separator = PHP_EOL)
+    {
+        if ($this->exists($path)) {
+            return $this->put($path, $this->get($path) . $separator . $data);
+        }
+
+        return $this->put($path, $data);
+    }
+
+    /**
+     * 删除文件
+     *
+     * @param  string|array $paths
+     * @return bool
+     */
+    public function delete($paths)
+    {
+        $paths = is_array($paths) ? $paths : func_get_args();
+        $success = true;
+        foreach ($paths as $path) {
+            try {
+                if (!$this->driver->delete($path)) {
+                    $success = false;
+                }
+            } catch (FileNotFoundException $e) {
+                $success = false;
+            }
+        }
+
+        return $success;
+    }
+
+    /**
+     * 复制文件
+     *
+     * @param  string $from
+     * @param  string $to
+     * @return bool
+     * @throws FileNotFoundException
+     * @throws \League\Flysystem\FileExistsException
+     */
+    public function copy($from, $to)
+    {
+        return $this->driver->copy($from, $to);
+    }
+
+    /**
+     * 移动文件
+     *
+     * @param  string $from
+     * @param  string $to
+     * @return bool
+     * @throws FileNotFoundException
+     * @throws \League\Flysystem\FileExistsException
+     */
+    public function move($from, $to)
+    {
+        return $this->driver->rename($from, $to);
+    }
+
+    /**
+     * 获取文件大小
+     *
+     * @param  string $path
+     * @return int
+     * @throws FileNotFoundException
+     */
+    public function size($path)
+    {
+        return $this->driver->getSize($path);
+    }
+
+    /**
+     * 获取文件的  mime-type
+     *
+     * @param  string $path
+     * @return string|false
+     * @throws FileNotFoundException
+     */
+    public function mimeType($path)
+    {
+        return $this->driver->getMimetype($path);
+    }
+
+    /**
+     * 获取文件最后修改时间
+     *
+     * @param  string $path
+     * @return int
+     * @throws FileNotFoundException
+     */
+    public function lastModified($path)
+    {
+        return $this->driver->getTimestamp($path);
+    }
+
+    /**
+     * 获取文件访问Url
+     *
+     * @param  string $path
+     * @return string
+     */
+    public function url($path)
+    {
+        $adapter = $this->driver->getAdapter();
+
+        if ($adapter instanceof CachedAdapter) {
+            $adapter = $adapter->getAdapter();
+        }
+
+        if (method_exists($adapter, 'getUrl')) {
+            return $adapter->getUrl($path);
+        } else {
+            throw new RuntimeException('This driver does not support retrieving URLs.');
+        }
+    }
+
+    /**
+     * 获取文件的临时访问Url
+     *
+     * @param  string $path
+     * @param  \DateTimeInterface $expiration
+     * @param  array $options
+     * @return string
+     */
+    public function temporaryUrl($path, $expiration, array $options = [])
+    {
+        $adapter = $this->driver->getAdapter();
+
+        if ($adapter instanceof CachedAdapter) {
+            $adapter = $adapter->getAdapter();
+        }
+
+        if (method_exists($adapter, 'getTemporaryUrl')) {
+            return $adapter->getTemporaryUrl($path, $expiration, $options);
+        } else {
+            throw new RuntimeException('This driver does not support creating temporary URLs.');
+        }
+    }
+
+    /**
+     * Concatenate a path to a URL.
+     *
+     * @param  string $url
+     * @param  string $path
+     * @return string
+     */
+    protected function concatPathToUrl($url, $path)
+    {
+        return rtrim($url, '/') . '/' . ltrim($path, '/');
+    }
+
+    /**
+     * Get an array of all files in a directory.
+     *
+     * @param  string|null $directory
+     * @param  bool $recursive
+     * @return array
+     */
+    public function files($directory = null, $recursive = false)
+    {
+        $contents = $this->driver->listContents($directory, $recursive);
+
+        return $this->filterContentsByType($contents, 'file');
+    }
+
+    /**
+     * Get all of the files from the given directory (recursive).
+     *
+     * @param  string|null $directory
+     * @return array
+     */
+    public function allFiles($directory = null)
+    {
+        return $this->files($directory, true);
+    }
+
+    /**
+     * Get all of the directories within a given directory.
+     *
+     * @param  string|null $directory
+     * @param  bool $recursive
+     * @return array
+     */
+    public function directories($directory = null, $recursive = false)
+    {
+        $contents = $this->driver->listContents($directory, $recursive);
+        return $this->filterContentsByType($contents, 'dir');
+    }
+
+    /**
+     * Get all (recursive) of the directories within a given directory.
+     *
+     * @param  string|null $directory
+     * @return array
+     */
+    public function allDirectories($directory = null)
+    {
+        return $this->directories($directory, true);
+    }
+
+    /**
+     * 创建文件夹
+     *
+     * @param  string $path
+     * @return bool
+     */
+    public function makeDirectory($path)
+    {
+        return $this->driver->createDir($path);
+    }
+
+    /**
+     * 删除文件夹
+     *
+     * @param  string $directory
+     * @return bool
+     */
+    public function deleteDirectory($directory)
+    {
+        return $this->driver->deleteDir($directory);
+    }
+
+    /**
+     * 刷新文件系统缓存
+     *
+     * @return void
+     */
+    public function flushCache()
+    {
+        $adapter = $this->driver->getAdapter();
+        if ($adapter instanceof CachedAdapter) {
+            $adapter->getCache()->flush();
+        }
+    }
+
+    /**
+     * 获取文件系统原始驱动
+     *
+     * @return \League\Flysystem\FilesystemInterface
+     */
+    public function getDriver()
+    {
+        return $this->driver;
+    }
+
+    /**
+     * 解析可见性值
+     *
+     * @param  string|null $visibility
+     * @return string
+     *
+     * @throws InvalidArgumentException
+     */
+    protected function parseVisibility($visibility)
+    {
+        if (is_null($visibility)) {
+            return;
+        }
+        switch ($visibility) {
+            case FilesystemInterface::VISIBILITY_PUBLIC:
+                return AdapterInterface::VISIBILITY_PUBLIC;
+            case FilesystemInterface::VISIBILITY_PRIVATE:
+                return AdapterInterface::VISIBILITY_PRIVATE;
+        }
+
+        throw new InvalidArgumentException('Unknown visibility: ' . $visibility);
+    }
+
+    /**
+     * Pass dynamic methods call onto Flysystem.
+     *
+     * @param  string $method
+     * @param  array $params
+     * @return mixed
+     *
+     * @throws \BadMethodCallException
+     */
+    public function __call($method, $params)
+    {
+        return call_user_func_array([$this->driver, $method], $params);
     }
 }
