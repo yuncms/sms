9778900ba6a671601683e0177707a032333a519c
XuTongle <xutongle@gmail.com>
2018-05-14 18:55:17 +0800

    update

--- src/filesystem/FilesystemManager.php
+++ src/filesystem/FilesystemManager.php
@@ -10,20 +10,7 @@ namespace yuncms\filesystem;
 use Closure;
 use Yii;
 use yii\base\Component;
-use yii\caching\Cache as YiiCache;
-use yii\base\InvalidArgumentException;
 use yii\base\InvalidConfigException;
-use yii\di\Instance;
-use yuncms\filesystem\contracts\Filesystem;
-use League\Flysystem\AdapterInterface;
-use League\Flysystem\FilesystemInterface;
-use League\Flysystem\Cached\CachedAdapter;
-use League\Flysystem\Filesystem as Flysystem;
-use League\Flysystem\Adapter\Ftp as FtpAdapter;
-use League\Flysystem\Rackspace\RackspaceAdapter;
-use League\Flysystem\Adapter\Local as LocalAdapter;
-use League\Flysystem\AwsS3v3\AwsS3Adapter as S3Adapter;
-use League\Flysystem\Cached\Storage\Memory as MemoryStore;
 
 /**
  * Class Storage
@@ -51,148 +38,240 @@ class FilesystemManager extends Component
     public $cloud = 'local';
 
     /**
-     * @var string|YiiCache
+     * @var array filesystem parameters (name => value).
      */
-    public $cache = 'cache';
+    public $params = [];
 
     /**
-     * @var string
+     * @var array shared disk instances indexed by their IDs
      */
-    public $cacheKey = 'filesystem';
+    private $_filesystems = [];
 
     /**
-     * @var integer
+     * @var array filesystem definitions indexed by their IDs
      */
-    public $cacheDuration = 0;
+    private $_definitions = [];
 
     /**
-     * The array of resolved filesystem drivers.
-     *
-     * @var array
+     * Getter magic method.
+     * This method is overridden to support accessing filesystems like reading properties.
+     * @param string $name filesystem or property name
+     * @return mixed the named property value
+     * @throws InvalidConfigException
+     * @throws \yii\base\UnknownPropertyException
      */
-    protected $disks = [];
-
-    /**
-     * The registered custom driver creators.
-     *
-     * @var array
-     */
-    protected $customCreators = [];
-
-    /**
-     * Attempt to get the disk from the local cache.
-     *
-     * @param  string $name
-     * @return Filesystem
-     */
-    protected function get($name)
+    public function __get($name)
     {
-        return $this->disks[$name] ?? $this->resolve($name);
+        if ($this->has($name)) {
+            return $this->get($name);
+        }
+
+        return parent::__get($name);
     }
 
     /**
-     * Resolve the given disk.
-     *
-     * @param  string $name
-     * @return Filesystem
-     *
-     * @throws InvalidArgumentException
+     * Checks if a property value is null.
+     * This method overrides the parent implementation by checking if the named filesystem is loaded.
+     * @param string $name the property name or the event name
+     * @return bool whether the property value is null
      */
-    protected function resolve($name)
+    public function __isset($name)
     {
-        $config = $this->disks[$name];
-        $driverMethod = 'create' . ucfirst($config['adapter']) . 'Adapter';
-        if (method_exists($this, $driverMethod)) {
-            return $this->{$driverMethod}($config);
+        if ($this->has($name)) {
+            return true;
+        }
+
+        return parent::__isset($name);
+    }
+
+    /**
+     * Returns a value indicating whether the locator has the specified filesystem definition or has instantiated the filesystem.
+     * This method may return different results depending on the value of `$checkInstance`.
+     *
+     * - If `$checkInstance` is false (default), the method will return a value indicating whether the locator has the specified
+     *   filesystem definition.
+     * - If `$checkInstance` is true, the method will return a value indicating whether the locator has
+     *   instantiated the specified filesystem.
+     *
+     * @param string $id filesystem ID (e.g. `local`).
+     * @param bool $checkInstance whether the method should check if the filesystem is shared and instantiated.
+     * @return bool whether the locator has the specified filesystem definition or has instantiated the filesystem.
+     * @see set()
+     */
+    public function has($id, $checkInstance = false)
+    {
+        return $checkInstance ? isset($this->_filesystems[$id]) : isset($this->_definitions[$id]);
+    }
+
+    /**
+     * Returns the filesystem instance with the specified ID.
+     *
+     * @param string $id filesystem ID (e.g. `db`).
+     * @param bool $throwException whether to throw an exception if `$id` is not registered with the locator before.
+     * @return FilesystemAdapter|object|null the filesystem of the specified ID. If `$throwException` is false and `$id`
+     * is not registered before, null will be returned.
+     * @throws InvalidConfigException if `$id` refers to a nonexistent filesystem ID
+     * @see has()
+     * @see set()
+     */
+    public function get($id, $throwException = true)
+    {
+        if (isset($this->_filesystems[$id])) {
+            return $this->_filesystems[$id];
+        }
+
+        if (isset($this->_definitions[$id])) {
+            $definition = $this->_definitions[$id];
+            if (is_object($definition) && !$definition instanceof Closure) {
+                return $this->_filesystems[$id] = $definition;
+            }
+
+            return $this->_filesystems[$id] = Yii::createObject($definition);
+        } elseif ($throwException) {
+            throw new InvalidConfigException("Unknown filesystem ID: $id");
+        }
+
+        return null;
+    }
+
+    /**
+     * Registers a filesystem definition with this locator.
+     *
+     * For example,
+     *
+     * ```php
+     * // a class name
+     * $locator->set('cache', 'yii\caching\FileCache');
+     *
+     * // a configuration array
+     * $locator->set('db', [
+     *     'class' => 'yii\db\Connection',
+     *     'dsn' => 'mysql:host=127.0.0.1;dbname=demo',
+     *     'username' => 'root',
+     *     'password' => '',
+     *     'charset' => 'utf8',
+     * ]);
+     *
+     * // an anonymous function
+     * $locator->set('cache', function ($params) {
+     *     return new \yii\caching\FileCache;
+     * });
+     *
+     * // an instance
+     * $locator->set('cache', new \yii\caching\FileCache);
+     * ```
+     *
+     * If a filesystem definition with the same ID already exists, it will be overwritten.
+     *
+     * @param string $id filesystem ID (e.g. `db`).
+     * @param mixed $definition the filesystem definition to be registered with this locator.
+     * It can be one of the following:
+     *
+     * - a class name
+     * - a configuration array: the array contains name-value pairs that will be used to
+     *   initialize the property values of the newly created object when [[get()]] is called.
+     *   The `class` element is required and stands for the the class of the object to be created.
+     * - a PHP callable: either an anonymous function or an array representing a class method (e.g. `['Foo', 'bar']`).
+     *   The callable will be called by [[get()]] to return an object associated with the specified filesystem ID.
+     * - an object: When [[get()]] is called, this object will be returned.
+     *
+     * @throws InvalidConfigException if the definition is an invalid configuration array
+     */
+    public function set($id, $definition)
+    {
+        unset($this->_filesystems[$id]);
+
+        if ($definition === null) {
+            unset($this->_definitions[$id]);
+            return;
+        }
+
+        if (is_object($definition) || is_callable($definition, true)) {
+            // an object, a class name, or a PHP callable
+            $this->_definitions[$id] = $definition;
+        } elseif (is_array($definition)) {
+            // a configuration array
+            if (isset($definition['class'])) {
+                $this->_definitions[$id] = $definition;
+            } else {
+                throw new InvalidConfigException("The configuration for the \"$id\" filesystem must contain a \"class\" element.");
+            }
         } else {
-            throw new InvalidArgumentException("Adapter [{$config['adapter']}] is not supported.");
+            throw new InvalidConfigException("Unexpected configuration type for the \"$id\" filesystem: " . gettype($definition));
         }
     }
 
     /**
-     * Adapt the filesystem implementation.
-     *
-     * @param  \League\Flysystem\FilesystemInterface  $filesystem
-     * @return Filesystem
+     * Removes the filesystem from the locator.
+     * @param string $id the filesystem ID
      */
-    protected function adapt(FilesystemInterface $filesystem)
+    public function clear($id)
     {
-        return new FilesystemAdapter($filesystem);
+        unset($this->_definitions[$id], $this->_filesystems[$id]);
     }
 
     /**
-     * Create a cache store instance.
+     * Returns the list of the filesystem definitions or the loaded filesystem instances.
+     * @param bool $returnDefinitions whether to return filesystem definitions instead of the loaded filesystem instances.
+     * @return array the list of the filesystem definitions or the loaded filesystem instances (ID => definition or instance).
+     */
+    public function getFilesystems($returnDefinitions = true)
+    {
+        return $returnDefinitions ? $this->_definitions : $this->_filesystems;
+    }
+
+    /**
+     * Registers a set of filesystem definitions in this locator.
      *
-     * @param  mixed $config
-     * @return \League\Flysystem\Cached\CacheInterface
+     * This is the bulk version of [[set()]]. The parameter should be an array
+     * whose keys are filesystem IDs and values the corresponding filesystem definitions.
      *
-     * @throws \InvalidArgumentException
+     * For more details on how to specify filesystem IDs and definitions, please refer to [[set()]].
+     *
+     * If a filesystem definition with the same ID already exists, it will be overwritten.
+     *
+     * The following is an example for registering two filesystem definitions:
+     *
+     * ```php
+     * [
+     *     'local' => [
+     *         'class' => 'yuncms\filesystem\adapters\LocalAdapter',
+     *         'path' => '@root/storage',
+     *     ],
+     * ]
+     * ```
+     *
+     * @param array $filesystems filesystem definitions or instances
      * @throws InvalidConfigException
      */
-    protected function createCacheStore($config)
+    public function setFilesystems($filesystems)
     {
-        if ($config === true) {
-            return new MemoryStore;
+        foreach ($filesystems as $id => $filesystem) {
+            $this->set($id, $filesystem);
         }
-        if ($this->cache !== null) {
-            $this->cache = Instance::ensure($config, YiiCache::class);
-            if (!$this->cache instanceof YiiCache) {
-                throw new InvalidConfigException('The "cache" property must be an instance of \yii\caching\Cache subclasses.');
-            }
-            return new Cache($this->cache, $this->cacheKey, $this->cacheDuration);
-        }
-        return new MemoryStore;
     }
 
     /**
-     * Create a Flysystem instance with the given adapter.
-     *
-     * @param  \League\Flysystem\AdapterInterface $adapter
-     * @param  array $config
-     * @return \League\Flysystem\FilesystemInterface
+     * 获取磁盘
+     * @param string|null $filesystem
+     * @return FilesystemAdapter
+     * @throws InvalidConfigException
      */
-    protected function createFlysystem(AdapterInterface $adapter, array $config)
+    public function cloud($filesystem = null)
     {
-
-
-        //$config = Arr::only($config, ['visibility', 'disable_asserts', 'url']);
-
-        if (isset($config['cache'])) {
-            $adapter = new CachedAdapter($adapter, $this->createCacheStore($config['cache']));
-        }
-
-        return new Flysystem($adapter, count($config) > 0 ? $config : null);
+        $filesystem = !is_null($filesystem) ? $filesystem : $this->cloud;
+        return $this->get($filesystem);
     }
 
     /**
-     * Create an instance of the local driver.
-     *
-     * @param  array $config
-     * @return FilesystemInterface
+     * 获取磁盘
+     * @param string|null $filesystem
+     * @return FilesystemAdapter
+     * @throws InvalidConfigException
      */
-    public function createLocalDriver(array $config)
+    public function disk($filesystem = null)
     {
-        $permissions = $config['permissions'] ?? [];
-
-        $links = ($config['links'] ?? null) === 'skip'
-            ? LocalAdapter::SKIP_LINKS
-            : LocalAdapter::DISALLOW_LINKS;
-
-        return $this->adapt($this->createFlysystem(new LocalAdapter(
-            $config['root'], LOCK_EX, $links, $permissions
-        ), $config));
-    }
-
-    /**
-     * Create an instance of the ftp driver.
-     *
-     * @param  array $config
-     * @return Filesystem
-     */
-    public function createFtpDriver(array $config)
-    {
-        return $this->adapt($this->createFlysystem(
-            new FtpAdapter($config), $config
-        ));
+        $filesystem = !is_null($filesystem) ? $filesystem : $this->default;
+        return $this->get($filesystem);
     }
 }
